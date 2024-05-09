8f642df4ea12aaf0fcd98ecc91de614837907e2d
NunoCosta <a20462@alunos.ipca.pt>
2024-05-09 10:18:08 +0100

    Alfa 1.1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 const express = require("express")
 const mongoose = require('mongoose');
+const path = require("path")
 
 const app = express()
 app.use(express.json())
 const port = 3000
 
-//Carros db = dados a preencher
+//DB = dados a preencher
+//Carros
 const Car = mongoose.model('Car', {
     marca: String,
     modelo: String,
-    //kms: Int16Array,
+    kms: Number,
     combustivel: String,
-    //ano: Int16Array,
-    mudancas: String,
-    //img: String,
-    //garantia: Int16Array,
+    ano: Number,
+    caixa: String,
+    img: String,
+    garantia: String,
+    preco: Number,
+    cor: String,
     });
+
+//Peças
+const Peca = mongoose.model('Peca', {
+    tipo: String,
+    preco: Number,
+    descricao: String,
+})
 
 //Listagem db = lista todos os carros 
 app.get("/", async (req, res) => {
@@ -23,46 +34,89 @@ app.get("/", async (req, res) => {
     return res.send(cars)
   })
 
-//Apagar db por id (:id) = permite apagar carros por id
+  app.get("/", async (req, res) => {
+    const pecas = await Peca.find()
+    return res.send(pecas)
+  })
+
+//Apagar db por id (:id)
+//Carro
 app.delete("/:id", async(req, res) => {
     const car = await Car.findByIdAndDelete(req.params.id)
     return res.send(car)
 })
 
-//Update db por id (:id) = permite atulizar dados por id
+//Peça
+app.delete("/:id", async(req, res) => {
+    const peca = await Car.findByIdAndDelete(req.params.id)
+    return res.send(peca)
+})
+
+
+//Update db por id (:id)
+//Carros
 app.put("/:id", async(req, res) => {
     const car = await Car.findByIdAndUpdate(req.params.id, {
         marca: req.body.marca,
         modelo: req.body.modelo,
-        //kms: req.body.kms,
+        kms: req.body.kms,
         combustivel: req.body.combustivel,
-        //ano: req.body.ano,
-        mudancas: req.body.mudancas,
-        //img: req.body.img,
-        //garantia: req.body.garantia,
+        ano: req.body.ano,
+        caixa: req.body.caixa,
+        img: req.body.img,
+        garantia: req.body.garantia,
+        preco: req.body.preco,
+        cor: req.body.cor,
     }, {
-        new: true
+        new: true //confirmação
     })
     return res.send(car)
 })
 
+//Pecas
+app.put("/:id", async(req, res) => {
+    const peca = await Peca.findByIdAndUpdate(req.params.id, {
+        tipo: req.body.tipo,
+        preco: req.body.preco,
+        descricao: req.body.descricao,
+    }, {
+        new: true //confirmação
+    })
+    return res.send(peca)
+})
+
+//Carro
 app.post("/", async (req, res) => {
     const car = new Car({
         marca: req.body.marca,
         modelo: req.body.modelo,
-        //kms: req.body.kms,
+        kms: req.body.kms,
         combustivel: req.body.combustivel,
-        //ano: req.body.ano,
-        mudancas: req.body.mudancas,
-        //img: req.body.img,
-        //garantia: req.body.garantia,
+        ano: req.body.ano,
+        caixa: req.body.caixa,
+        img: req.body.img,
+        garantia: req.body.garantia,
+        preco: req.body.preco,
+        cor: req.body.cor,
     })
     await car.save()
     res.send(car)
 })
 
+//Criar - DB
+//Peca
+app.post("/", async (req, res) => {
+    const peca = new Peca({
+        tipo: req.body.tipo,
+        preco: req.body.preco,
+        descricao: req.body.descricao,
+    })
+    await peca.save()
+    res.send(peca)
+})
+
 //Conexão para MongoDB
 app.listen(port, () => {
-        mongoose.connect('mongodb+srv://NunoCosta99:3vfaIMGMttvnmYJx@scfautos-api.ynmru2m.mongodb.net/?retryWrites=true&w=majority&appName=SCFAutos-api')
+        mongoose.connect('mongodb+srv://NunoCosta99:YBvfZ1kBvhGGZOxF@scfautos-api.ynmru2m.mongodb.net/?retryWrites=true&w=majority&appName=SCFAutos-api')
         console.log('App running')
   })
